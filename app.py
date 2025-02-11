9ab478fcfa52adcb8673d40ac8e7621380f0851f
MrArmah <niiarmahgg@gmail.com>
2025-02-11 14:30:55 +0000

    Database Update

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from dotenv import load_dotenv
 
 
 ## APP setup
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
 
 
 #URLs for APIs
@@ -159,7 +160,7 @@ class My_Tools(db.Model):
 if __name__ in "__main__":
     with app.app_context():
         db.create_all()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 6005))
     app.run(host='0.0.0.0', port=port, debug=True)
 
 
